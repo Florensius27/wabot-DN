089539710bdafd31699c6b7180ab43fc8de51d40
Florensius27 <78130347+Florensius27@users.noreply.github.com>
2021-02-06 20:27:54 +0700

    Create

--- Tutorial.js
+++ Tutorial.js
@@ -1,22 +1,20 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   conn.reply(m.chat, `
 Tutorial Buat Bot WA:
-Credit: *~Florensius*
+Credit: *~Floren*
 
 User TERMUX Android
 1) $pkg install git
-2) $pkg install ffmpeg
-3) $pkg install imagemagick
-4) $pkg install nodejs
+2) $pkg install nodejs
+3) $pkg install ffmpeg
+4) $pkg install imagemagick
 5) $git clone https://github.com/Florensius27/Loren-BOT
-6) $ls
 7) $cd Loren-BOT
-8) $npm i
-9) $node index.js
+8) $npm install
 
 note : lambang *$* tidak usah ditulis!
 
-User WINDOWS/VPS/RDP dll
+User WINDOWS/VPS/RDP
 1) git clone https://github.com/Florensius27/Loren-BOT
 2) cd Loren-BOT
 3) npm install
