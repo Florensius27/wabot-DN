2602cd3ee08e6e048fe734f3e6e96f57cbb0728e
Florensius27 <78130347+Florensius27@users.noreply.github.com>
2021-02-11 21:42:08 +0700

    Update

--- Fakta.js
+++ Fakta.js
@@ -22,7 +22,7 @@ function pickRandom(list) {
 }
 "Gajah adalah satu-satunya hewan mamalia darat yang tidak dapat melompat dan meloncat, karena struktur kaki gajah tidak untuk melompat dan meloncat, melainkan untuk menahan bobot tubuhnya yang bisa mencapai 2.700 kg hingga 6.000 kg!"
 "Paus biru adalah hewan terbesar saat ini, paus biru dapat memilik berat sekitar 50.000 kg hingga 150.000 kg atau 181 ton, dengan panjang tubuh yang mencapai 33 meter, meskipun memiliki ukuran tubuh yang sangat besar, paus biru dapat berenang dengan kecepatan 20 km/jam sampai dengan 50 km/jam, dan paus tidak termasuk ikan!"
-"Boba muncul pertama kali di Taiwan sekitar tahun 1980an di sebuah kedai teh, boba ditemukan tak sengaja dari hasil iseng dengan mencampurkan fen yuan kedalam teh dingin,
+"Boba muncul pertama kali di Taiwan sekitar tahun 1980an di sebuah kedai teh, boba ditemukan tak sengaja dari hasil iseng dengan mencampurkan fen yuan kedalam teh dingin"
 "Tikus adalah hewan dengan tubuh paling fleksibel, tikus dewasa saja dapat melewati lubang yang berdiameter kurang lebih 2 cm, itu sebabnya tikus dapat bersembunyi atau datang dari mana saja dengan sesuka hati, tak hanya fleksibel, tikus juga memiliki kecerdasan yang luar biasa!"
 "Kodok yang dapat mendeteksi gempa adalah kodok L'Aqila, kodok L'Aqila adalah satu-satunya kodok yang dapat mendeteksi gempa"
 "Ayam adalah keturunan langsung dari Tyrannosaurus rex, dan ayam memiliki jumlah yang jauh melebihi jumlah manusia yang hidup dibumi yaitu 19 miliar lebih, sedangkan manusia hanya 7 miliar (memurut data tahun 2011)"
