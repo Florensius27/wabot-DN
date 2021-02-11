8800dc307f3f9d1da1babd1ff4f5e5cfb6927305
Florensius27 <78130347+Florensius27@users.noreply.github.com>
2021-02-11 11:09:46 +0700

    Update

--- Font.js
+++ Font.js
@@ -1,1 +1,1 @@
-https://drive.google.com/file/d/1CqKFxcI64SPRXmhZ5jgq0gwog7CJyFiN/view?usp=drivesdk
+<p align="center">https://drive.google.com/file/d/1CqKFxcI64SPRXmhZ5jgq0gwog7CJyFiN/view?usp=drivesdk
