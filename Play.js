c57a2921495e8c5755284d8d02404af622a23318
Florensius27 <78130347+Florensius27@users.noreply.github.com>
2021-02-16 11:12:33 +0700

    Create

--- Play.js
+++ Play.js
@@ -3,12 +3,14 @@ let handler = async function (m, { text, isPrems, isOwner }) {
   if (!text) throw 'Tidak ada yang dicari'
   let res = await fetch(global.API('xteam', '/dl/play', { lagu: text }, 'APIKEY'))
   let { judul, size, thumbnail, url } = await res.json()
+
+   m.reply('*[_wᴀɪᴛ_] _sᴇᴅᴀɴɢ memᴩʀᴏꜱᴇꜱ...⏳_*')
   let s = Number(size)
   s = /MB/.test(size) ? s * 1024 : s
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < s
   this.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
 *Title:* ${judul}
-*Filesize:* ${size}
+*size:* ${size}
 'Pakai ': ''}Link:* ${url}
 `.trim(), m)
   this.sendFile(m.chat, url, 'audio.mp3', `
@@ -16,7 +18,7 @@ let handler = async function (m, { text, isPrems, isOwner }) {
 *Filesize:* ${size}
 `.trim(), m)
 }
-handler.help = ['play <pencarian>']
+handler.help = ['play [search]']
 handler.tags = ['downloader']
 handler.command = /^play$/i
 
